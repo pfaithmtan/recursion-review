42e46ca9c0663a63e63254fd8d3db14f81fa12e6
pfaithmtan <pfaithmtan@gmail.com>
2019-09-03 11:49:07 -0700

    Complete base (primitive) cases

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,34 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  } else if (typeof obj === 'number' || typeof obj === 'boolean' || obj === null) {
+    return "" + obj;
+  }
+
   // your code goes here
+  //base cases:
+  //if starts with quote...
+    //return '"' + obj + '"'
+  //if typeof obj is number...
+    //return "" + obj
+  //if typeof obj is boolean...
+    //return "" + obj
+  //if item is null...
+    //return "" + null
+
+  //if item starts with [, ends with ]...
+    //var result = [];
+    //for each ele...
+      //result.push(stringifyJSON(ele))
+    //return '[' + result + ']'
+
+  //if item starts with '{' ends with }...
+    //var result = [];
+    // for in ...
+      //var pair = stringifyJSON(key) + ':' + stringifyJSON(obj[key])
+      //result.push(pair)
+
+
 };
