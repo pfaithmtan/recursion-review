5090ee683af6a359eb7c4ee3952a4ab4caeda45e
pfaithmtan <pfaithmtan@gmail.com>
2019-09-03 15:54:08 -0700

    Start implementation of code
    - Write out pseudocode

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,46 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className, node) {
+  if (node === undefined) {
+    node = document.body;
+  }
+
+  var result = [];
+  var arrayList = node.classList;
+
+  // console.log(arrayList);
+
+  if (arrayList !== undefined) {
+    arrayList.forEach(function(ele) {
+      if (ele === className) {
+        result.push(node);
+      }
+    });
+  }
+
+  if (node.hasChildNodes) {
+    const childNodeArr = node.childNodes;
+
+    childNodeArr.forEach(function(ele) {
+      getElementsByClassName(className, ele);
+    });
+  }
+
+  return result;
   // your code here
+  //if node is undefined...
+  //node = document.body
+
+  //declare result array
+  //iterate through node.classlist (an 'array' of classlist)
+  //for each ele, if equals to ClassName
+  //push element into result
+
+  //check if node has childNodes...
+  //for each childNodes...
+  //yes: getElementbyClassName(className, childnode);
+
+  //return result
+
 };
